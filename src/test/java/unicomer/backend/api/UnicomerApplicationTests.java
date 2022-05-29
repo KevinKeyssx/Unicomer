348d78c8c26c8c46f9a83862704650356830ca0d
Keyss <55340042+KevinKeyssx@users.noreply.github.com>
2022-05-28 20:32:06 -0400

    initial commit

---
+++ src/test/java/unicomer/backend/api/UnicomerApplicationTests.java
@@ -0,0 +1,13 @@
+package unicomer.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
